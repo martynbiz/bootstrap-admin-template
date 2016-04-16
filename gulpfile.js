9f17c2955b5658974d63c3e3c050ff9b3597a81d
Martyn Bissett <m@rtyn.biz>
2016-04-16 11:50:37 +0900

    Make js work

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    // minifyCss = require('gulp-minify-css'),
+    minifyCss = require('gulp-minify-css'),
     sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
     watch = require('gulp-watch');
 
 // task for compiling less, combining css, and minifying
@@ -11,17 +12,43 @@ gulp.task('css', function () {
     gulp.src('less/admin.less')
         .pipe(sourcemaps.init())  // Process the original sources
         .pipe(less())
+        .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(sourcemaps.write()) // Add the map to modified source.
         .pipe(gulp.dest('css'));
 
 });
 
+// task for compiling less, combining css, and minifying
+gulp.task('js', function () {
+
+    // complile frontend js
+    return gulp.src([
+            // 'js/bootstrap/affix.js',
+            // 'js/bootstrap/alert.js',
+            // 'js/bootstrap/button.js',
+            // 'js/bootstrap/carousel.js',
+            'js/bootstrap/collapse.js',
+            'js/bootstrap/dropdown.js',
+            'js/bootstrap/modal.js',
+            // 'js/bootstrap/popover.js',
+            // 'js/bootstrap/scrollspy.js',
+            // 'js/bootstrap/tab.js',
+        ])
+        .pipe(concat('bootstrap.js'))
+        .pipe(gulp.dest('js'));
+
+});
+
+
+
 gulp.task('default', function() {
     gulp.start('css');
+    gulp.start('js');
 });
 
 gulp.task('watch', function () {
     watch(['less/**/*.less'], function (event) {
         gulp.start('css');
+        gulp.start('js');
     });
 });
